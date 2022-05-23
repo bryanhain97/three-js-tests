42c15ebf9b1a4d170420f295c4ce864b36d1b4bb
Bryan Hain <bryanhain97@gmail.com>
2022-05-23 20:57:17 +0200

    clean plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,14 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 const EslintWebpackPlugin = require('eslint-webpack-plugin')
+const path = require('path')
+
 module.exports = merge(common, {
   mode: "development",
   devServer: {
     watchFiles: ["./src/**/*"],
-    static: './dist',
+    static: path.resolve(__dirname, 'src'),
+    open: true
   },
   plugins: [
     new EslintWebpackPlugin(),
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,36 @@
 const { merge } = require('webpack-merge')
+const path = require('path')
 const common = require('./webpack.common')
 const CopyPlugin = require('copy-webpack-plugin')
 const WriteFileWebpackPlugin = require('write-file-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
-const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
   mode: "production",
+  output: {
+    path: path.resolve(process.cwd(), 'dist')
+  },
   plugins: [
     new CopyPlugin({
-        patterns: [
-            {
-              from: './src/assets/fonts',
-              to: './assets/fonts/',
-              context: path.resolve(__dirname),
-              force: true,
-            }
-          ]
+      patterns: [
+        {
+          from: './src/assets/fonts',
+          to: './assets/fonts/',
+          context: path.resolve(__dirname),
+          force: true,
+        }
+      ]
     }),
     new WriteFileWebpackPlugin(),
     new CssMinimizerPlugin(),
+    new CleanWebpackPlugin()
   ],
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(),
-      new TerserPlugin()
+      new TerserPlugin(),
     ],
   },
 })
